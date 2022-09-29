892ef7e8029e94df0fb616f898e81ebe3d86274e
Supawit Uttra <supawit.u@gpf.or.th>
2022-09-29 18:01:32 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace spw_first_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
